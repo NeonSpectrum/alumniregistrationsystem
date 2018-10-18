bfbe022e1b6da00932022382d719884368d75aad
NeonSpectrum <youngskymann@gmail.com>
2018-10-18 11:27:09 +0800

    added note to ticket

--- resources/views/ticket-mail.blade.php
+++ resources/views/ticket-mail.blade.php
@@ -1,3 +1,5 @@
 Dear {{ $name }},
 <br><br>
-This is your ticket.
+This is your e-ticket.
+
+<b>Note:</b> Every ticket is randomly generated and distinct with each other. Reproduction and/or redistribution of this ticket is strictly prohibited.
