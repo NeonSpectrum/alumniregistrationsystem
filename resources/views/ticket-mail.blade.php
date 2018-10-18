0d93c1c2cdcee20b2797397fadf245ea44b760a2
NeonSpectrum <youngskymann@gmail.com>
2018-10-18 11:28:52 +0800

    update ticket mail

--- resources/views/ticket-mail.blade.php
+++ resources/views/ticket-mail.blade.php
@@ -1,5 +1,5 @@
 Dear {{ $name }},
 <br><br>
-This is your e-ticket.
-
+This is to confirm your registration and payment. The attached file is your e-ticket.
+<br><br>
 <b>Note:</b> Every ticket is randomly generated and distinct with each other. Reproduction and/or redistribution of this ticket is strictly prohibited.
