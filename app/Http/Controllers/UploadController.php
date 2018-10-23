213621d8c9de5b25ad61b779347abfc3bc8bfc33
neonspectrum <youngskymann@gmail.com>
2018-10-23 15:04:32 +0800

    update

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -40,11 +40,11 @@ class UploadController extends Controller {
     $reference_number = Common::decrypt($request->code);
 
     try {
-      $users = \DB::table('users')->where('reference_number', $reference_number)->update([
+      $user = \DB::table('users')->where('reference_number', $reference_number)->update([
         'reference_file_name' => $filename
       ]);
 
-      \Mail::to('youngskymann@gmail.com')->send(new SendPictureMail($user->first_name . ' ' . $user->last_name, $request->file));
+      \Mail::to('youngskymann@gmail.com')->send(new SendPictureMail($user[0]->first_name . ' ' . $user[0]->last_name, $request->file));
     } catch (QueryException $e) {
       return json_encode(['success' => false, 'error' => $e]);
     }
