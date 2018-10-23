17e597b7a78de449478e9cc4ab0dd01d43626f9d
neonspectrum <youngskymann@gmail.com>
2018-10-23 15:41:50 +0800

    oops

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -48,7 +48,7 @@ class UploadController extends Controller {
 
       $user = \DB::table('users')->where('reference_number', $reference_number)->first();
 
-      \Mail::to('youngskymann@gmail.com')->send(new SendPictureMail($user->first_name . ' ' . $user->last_name, public_path('references') . '\\' . $filename, $mime));
+      \Mail::to('youngskymann@gmail.com')->send(new SendPictureMail($user->first_name . ' ' . $user->last_name, public_path('references') . '/' . $filename, $mime));
     } catch (QueryException $e) {
       return json_encode(['success' => false, 'error' => $e]);
     }
