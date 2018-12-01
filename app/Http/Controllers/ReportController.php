cfb96f202c45aae464e06464b91abd1478dc32d4
neonspectrum <youngskymann@gmail.com>
2018-12-01 15:24:00 +0800

    fix indention

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,31 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller {
-	/**
-	 * @return mixed
-	 */
-	protected function show() {
-		$users = \DB::table('users')->where('sent', 1)->get();
+  /**
+   * @return mixed
+   */
+  protected function show() {
+    $users = \DB::table('users')->where('sent', 1)->get();
 
-		foreach ($users as $user) {
-			$companions = \DB::table('companions')->where('id', $user->id)->get();
+    foreach ($users as $user) {
+      $companions = \DB::table('companions')->where('id', $user->id)->get();
 
-			$names = [];
+      $names = [];
 
-			foreach ($companions as $companion) {
-				$names[] = $companion->first_name . ' ' . $companion->last_name;
-			}
+      foreach ($companions as $companion) {
+        $names[] = $companion->first_name . ' ' . $companion->last_name;
+      }
 
-			$user->companions = [
-				'count' => $companions->count(),
-				'names' => join('<br>', $names)
-			];
+      $user->companions = [
+        'count' => $companions->count(),
+        'names' => join('<br>', $names)
+      ];
 
-			$user->date_sent = \DB::table('logs')->whereRaw('action LIKE "%Ticket%' . $user->email_address . '%"')->latest()->first()->created_at;
-		}
+      $user->date_sent = \DB::table('logs')->whereRaw('action LIKE "%Ticket%' . $user->email_address . '%"')->latest()->first()->created_at;
+    }
 
-		$pdf = \PDF::loadView('pdf.report', ['data' => $users]);
+    $pdf = \PDF::loadView('pdf.report', ['data' => $users]);
 
-		return $pdf->stream();
-	}
+    return $pdf->stream();
+  }
 }
--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,85 +8,85 @@ use Illuminate\Http\Request;
 use \Illuminate\Database\QueryException;
 
 class UploadController extends Controller {
-	/**
-	 * @param Request $request
-	 */
-	protected function showSuccess(Request $request) {
-		if ($request->session()->has('upload')) {
-			return view('upload-success');
-		} else {
-			return redirect()->route('register');
-		}
-	}
+  /**
+   * @param Request $request
+   */
+  protected function showSuccess(Request $request) {
+    if ($request->session()->has('upload')) {
+      return view('upload-success');
+    } else {
+      return redirect()->route('register');
+    }
+  }
 
-	/**
-	 * @param Request $request
-	 */
-	protected function create(Request $request) {
-		$code = $request->query('code', null);
+  /**
+   * @param Request $request
+   */
+  protected function create(Request $request) {
+    $code = $request->query('code', null);
 
-		if (!$code) {
-			abort(404);
-		}
+    if (!$code) {
+      abort(404);
+    }
 
-		$reference_number = Common::decrypt($code);
+    $reference_number = Common::decrypt($code);
 
-		$user = \DB::table('users')->where('reference_number', $reference_number)->first();
+    $user = \DB::table('users')->where('reference_number', $reference_number)->first();
 
-		if ($user) {
-			return view('upload', ['user' => $user, 'code' => $code]);
-		}
+    if ($user) {
+      return view('upload', ['user' => $user, 'code' => $code]);
+    }
 
-		abort(404);
-	}
+    abort(404);
+  }
 
-	/**
-	 * @param Request $request
-	 */
-	protected function store(Request $request) {
-		$filename = time() . '.' . $request->file->getClientOriginalExtension();
+  /**
+   * @param Request $request
+   */
+  protected function store(Request $request) {
+    $filename = time() . '.' . $request->file->getClientOriginalExtension();
 
-		$mime = $request->file->getMimeType();
+    $mime = $request->file->getMimeType();
 
-		if (substr($mime, 0, 5) != 'image') {
-			return json_encode(['success' => false, 'error' => 'This is not an image.']);
-		}
+    if (substr($mime, 0, 5) != 'image') {
+      return json_encode(['success' => false, 'error' => 'This is not an image.']);
+    }
 
-		$reference_number = Common::decrypt($request->code);
+    $reference_number = Common::decrypt($request->code);
 
-		$arr = [
-			'reference_file_name' => $filename
-		];
+    $arr = [
+      'reference_file_name' => $filename
+    ];
 
-		if ($request->data) {
-			parse_str($request->data, $data);
+    if ($request->data) {
+      parse_str($request->data, $data);
 
-			if (isset($data['nickname'])) {
-				$arr['nickname'] = $data['nickname'];
-			}
-			if (isset($data['batch'])) {
-				$arr['batch'] = $data['batch'];
-			}
-			if (isset($data['referrer'])) {
-				$arr['referrer'] = $data['referrer'];
-			}
-		}
+      if (isset($data['nickname'])) {
+        $arr['nickname'] = $data['nickname'];
+      }
+      if (isset($data['batch'])) {
+        $arr['batch'] = $data['batch'];
+      }
+      if (isset($data['referrer'])) {
+        $arr['referrer'] = $data['referrer'];
+      }
+    }
 
-		try {
-			\DB::table('users')->where('reference_number', $reference_number)->update($arr);
+    try {
+      \DB::table('users')->where('reference_number', $reference_number)->update($arr);
 
-			$user = \DB::table('users')->where('reference_number', $reference_number)->first();
+      $user = \DB::table('users')->where('reference_number', $reference_number)->first();
 
-			\Mail::to('aseret_f@yahoo.com')->send(new SendPictureMail($user->first_name . ' ' . $user->last_name, public_path('references') . '/' . $filename, $mime));
-			Common::createLog("Deposit slip of {$reference_number} has been sent to aseret_f@yahoo.com");
+      \Mail::to('aseret_f@yahoo.com')->send(new SendPictureMail($user->first_name . ' ' . $user->last_name, public_path('references') . '/' . $filename, $mime));
+      Common::createLog("Deposit slip of {$reference_number} has been sent to aseret_f@yahoo.com");
 
-			$request->session()->flash('upload', true);
+      $request->session()->flash('upload', true);
 
-			return json_encode(['success' => true]);
-		} catch (QueryException $e) {
-			return json_encode(['success' => false, 'error' => $e]);
-		} catch (\Exception $e) {
-			return json_encode(['success' => false, 'error' => $e->getMessage()]);
-		}
-	}
+      return json_encode(['success' => true]);
+    } catch (QueryException $e) {
+      return json_encode(['success' => false, 'error' => $e]);
+    } catch (\Exception $e) {
+      return json_encode(['success' => false, 'error' => $e->getMessage()]);
+    }
+  }
 }
