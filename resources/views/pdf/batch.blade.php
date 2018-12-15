59cf209160b22851fbb27ae141f0f3fe35043144
neonspectrum <youngskymann@gmail.com>
2018-12-15 20:03:44 +0800

    update raffle

--- resources/views/pdf/batch.blade.php
+++ resources/views/pdf/batch.blade.php
@@ -1,4 +1,19 @@
 @php($batch = 0)
+@php
+$excluded = [
+  "Sheila Geronimo",
+  "Ma. Teresa Borebor",
+  "Marivic Gatus",
+  "EDMON TORRES"
+  "Roselle Basa",
+  "ARNE BANA",
+  "ARIEL AVILES",
+  "SHIRLEY SHIRLEY",
+  "JOVY AFABLE",
+  "Sheila Geronimo",
+  "Rex Bringula"
+]
+@endphp
 <table width="100%">
   <thead>
     <tr>
@@ -9,7 +24,7 @@
   <tbody>
     @foreach($data as $row)
       @if($row->batch != 0)
-        <tr>
+        <tr>,
           <td>{{ $batch !== $row->batch ? $row->batch : '' }}</td>
           <td>{{ $row->first_name . " " . $row->last_name }}</td>
         </tr>
