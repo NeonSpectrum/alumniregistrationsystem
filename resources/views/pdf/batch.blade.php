98c9961db6829a369c58a726177dd1debffd331b
neonspectrum <youngskymann@gmail.com>
2018-12-15 22:56:51 +0800

    remove

--- resources/views/pdf/batch.blade.php
+++ resources/views/pdf/batch.blade.php
@@ -1,19 +1,4 @@
 @php($batch = 0)
-@php
-$excluded = [
-  "Sheila Geronimo",
-  "Ma. Teresa Borebor",
-  "Marivic Gatus",
-  "EDMON TORRES"
-  "Roselle Basa",
-  "ARNE BANA",
-  "ARIEL AVILES",
-  "SHIRLEY SHIRLEY",
-  "JOVY AFABLE",
-  "Sheila Geronimo",
-  "Rex Bringula"
-]
-@endphp
 <table width="100%">
   <thead>
     <tr>
@@ -24,7 +9,8 @@ $excluded = [
   <tbody>
     @foreach($data as $row)
       @if($row->batch != 0)
-        <tr>,
+        if($batch)
+        <tr>
           <td>{{ $batch !== $row->batch ? $row->batch : '' }}</td>
           <td>{{ $row->first_name . " " . $row->last_name }}</td>
         </tr>
