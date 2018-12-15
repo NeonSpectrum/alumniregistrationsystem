f8c9a22b988aade9d99abd4dac681499039bcc80
neonspectrum <youngskymann@gmail.com>
2018-12-15 17:44:21 +0800

    update

--- public/js/raffle.js
+++ public/js/raffle.js
@@ -159,7 +159,9 @@ var pickName = function() {
     })
   } else {
     choices = $(choices[0].dom)
-    $.post('raffle', { ref: choices.data('ref') })
+    $.ajax({
+      data: { ref: choices.data('ref') }
+    })
     imported = imported.filter(function(index) {
       return index.reference_number != choices.data('ref')
     })
