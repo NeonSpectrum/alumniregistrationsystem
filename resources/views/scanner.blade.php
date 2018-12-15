2938be3ad2b60af5a49ccec90d4988434f432ac7
neonspectrum <youngskymann@gmail.com>
2018-12-15 17:17:27 +0800

    add instructions

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -25,6 +25,7 @@
     <div style="height:500px;width:500px;overflow:hidden">
       <video id="preview" height="500px" width="500px" style="object-fit:cover"></video>
     </div>
+    <h1>Please scan your e-ticket QR code.</h1>
   </div>
 </div>
 @else
