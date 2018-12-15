c70d46600e73e3501a19a733776963df62763d06
neonspectrum <youngskymann@gmail.com>
2018-12-15 15:35:04 +0800

    update height width

--- public/js/raffle.js
+++ public/js/raffle.js
@@ -173,7 +173,7 @@ var pickName = function() {
       html: `
         <div style="font-size:85px;color:orange;font-weight:bold">${name}</div>
         <br>
-        <img src="img/ticket.png" alt="" height="500px" width="500px" style="object-fit:cover" />
+        <img src="img/ticket.png" alt="" height="1000px" width="1000px" style="object-fit:cover" />
         <div style="font-size:55px;color:orange;font-weight:bold">Congratulations!</div>
       `,
       customClass: 'swal2-modal-md',
